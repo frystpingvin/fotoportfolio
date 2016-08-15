8931a1fe4299e7bb48f11da7ea1fee56644191cb
Samuel Lie <samuel.lie1@gmail.com>
2016-08-15 15:20:03 +0200

    Add API to get photos from IG

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,29 +1,79 @@
 // This is where it all goes :)
-var pagesLoaded = 0;
+var pagesLoaded   = 0,
+    loadingImages = true,
+    instagramApi  = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=41682531.1677ed0.0345e6d7b61142afab3a948804bafcdc&count=4&callback=?';
 
 var getPhotos = function() {
-  var apiUrl = 'https://api.flickr.com/services/rest/?&method=flickr.people.getPublicPhotos&api_key=d608c9f0fefb8bda99a8ca1d3d8726b1&user_id=61937047@N07&per_page=8&page=' + (pagesLoaded + 1) + '&format=json&jsoncallback=?';
+  console.log('pages loaded: ' + pagesLoaded);
+  var flickrApi     = 'https://api.flickr.com/services/rest/?&method=flickr.people.getPublicPhotos&api_key=d608c9f0fefb8bda99a8ca1d3d8726b1&user_id=61937047@N07&&extras=date_upload&per_page=4&page=' + (pagesLoaded + 1) + '&format=json&jsoncallback=?',
+      photoArray    = [],
+      flickrPhotos,
+      instagramPhotos;
 
-  $.getJSON(apiUrl,
-    function(data) {
-      $.each(data.photos.photo, function(i, photo){
-        var src = 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '_h.jpg';
-        $('.do-gallery').append('<img src=' + src + ' alt=' + photo.title + ' ></div>')
+  console.log('insta api url: ' + instagramApi);
+
+  $.when(
+    $.getJSON(flickrApi, function(data) {
+      flickrPhotos = data;
+    }),
+    $.getJSON(instagramApi, function(data) {
+      instagramPhotos = data;
+    })
+  ).then(function() {
+
+    // create flickr photo objects
+    if (flickrPhotos) {
+      $.each(flickrPhotos.photos.photo, function(i, photo){
+        var photoObject = {};
+        photoObject.url = 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '_b.jpg';
+        photoObject.date = photo.dateupload;
+        photoArray.push(photoObject)
       });
     }
-  );
+    else {
+      console.log('flickr photos failed to load');
+    }
 
-  pagesLoaded++;
-  console.log(apiUrl);
+    // create ig photo objects
+    if (instagramPhotos) {
+      var photoSize   = "/s150x150/",
+          regExp      = /\/c[0-9]{3}\.[0-9].[0-9]{3}\.[0-9]{3}\//,
+          instagramApi = instagramPhotos.pagination.next_url + '&callback=?';
+
+      $.each(instagramPhotos.data, function(i, photo) {
+        var photoObject = {};
+        // The first and second line under this should be probably be more effective
+        var url = photo.images.thumbnail.url.replace(photoSize, '/');
+        photoObject.url   = url.replace(regExp, '/');
+        photoObject.date  = photo.created_time;
+        photoArray.push(photoObject);
+      });
+    }
+    else {
+      console.log('ig photos failed to load');
+    }
+
+    $.each(photoArray, function(i, photo) {
+      $('.do-gallery').append('<img src=' + photo.url + '></div>');
+    });
+
+    loadingImages = false;
+    $('loading').hide();
+    pagesLoaded++;
+  });
 };
 
-$(function() {
-  getPhotos()
-});
+getPhotos();
+
+console.log('this runs before GET is done');
 
 $(window).scroll(function() {
-  if($(window).scrollTop() + $(window).height() == $(document).height()) {
+  if( loadingImages == false && $(window).scrollTop() + $(window).height() == $(document).height() ) {
     console.log('Scrollbottom');
-    getPhotos()
+
+    loadingImages = true;
+    $('.loading').show();
+
+    getPhotos();
   }
 });
